6eb02f54080b8209e2287471996b3cbb73875047
HaibaraAi <1332126662@qq.com>
2023-07-05 11:01:34 +0800

    update

--- Test/libout.cpp
+++ Test/libout.cpp
@@ -1,8 +1,13 @@
 #include <LuckCoroutine.hpp>
 
+#include <iostream>
+
 int main()
 {
     luck::co::Info info;
+
+    ::std::cout << info.InfoString() << ::std::endl;
+    
     if (info.InfoString() != "$null")
         return -1;
     else
