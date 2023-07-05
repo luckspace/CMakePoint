565a0e48abd97554cace8c34887634f17dba1596
HaibaraAi <1332126662@qq.com>
2023-07-05 11:13:49 +0800

    correct

--- Include/LuckCoroutine.hpp
+++ Include/LuckCoroutine.hpp
@@ -2,10 +2,16 @@
 //  std
 #include <string>
 
+#if     defined(_WIN64)
 #ifndef LUCK_COROUTINE_BUILD
 #define LUCK_COROUTINE_API __declspec(dllimport)
 #else
 #define LUCK_COROUTINE_API __declspec(dllexport)
+#endif
+#elif   defined(__linux__)
+#define LUCK_COROUTINE_API
+#else
+#define LUCK_COROUTINE_API
 #endif
 
 namespace luck::co
